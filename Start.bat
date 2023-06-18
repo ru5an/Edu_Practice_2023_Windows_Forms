06be296ae0c4cadcda1dd142304cea1295d48280
ru5an <kolomoets.ruslan@gmail.com>
2023-06-18 21:55:57 +0200

    added in Start.bat dir Release DataPreparation

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
-cd .\Data_preparation\DataPreparation\x64\Debug\
+cd .\Data_preparation\DataPreparation\x64\Release\
 DataPreparation.exe data1.txt -10 -7 data2.txt -6 -5 data3.txt -5 -3 data4.txt -10 10
 cd ..\..\..\..\
 cd .\Output_of_graphs\OutputOfGraphs\OutputOfGraphs\bin\Release\
